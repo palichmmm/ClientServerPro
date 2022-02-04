2db467fce398015fc85002e5996c041d19877888
Евгений Козловский <palichmmm@gmail.com>
2022-02-04 18:58:14 +0300

    Поправки решения задачи

--- src/Client.java
+++ src/Client.java
@@ -4,42 +4,34 @@ import java.net.Socket;
 public class Client {
     private static final String HOST = "netology.homework";
     private static int port = 8080;
-    private static Socket clientSocket;
-    private static BufferedReader reader;
-    private static PrintWriter out;
-    private static BufferedReader in;
 
     public static void main(String[] args) {
-        try {
-            try {
-                clientSocket = new Socket(HOST, port);
-                reader = new BufferedReader(new InputStreamReader(System.in));
-                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                while(!clientSocket.isOutputShutdown()) {
-                    if(in.read() > -1) {
-                        String serv = in.readLine();
+        try (Socket clientSocket = new Socket(HOST, port);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            String text = "";
+            String serv;
+            while (!clientSocket.isOutputShutdown()) {
+                if (clientSocket.getInputStream().available() > 0) {
+                    serv = in.readLine();
+                    System.out.println(serv);
+                    text = reader.readLine();
+                    out.println(text);
+                }
+                if (text.equalsIgnoreCase("q")) {
+                    Thread.sleep(200); // Даем немного времени на ответ. Не успел - досвидание)
+                    if (clientSocket.getInputStream().available() > 0) {
+                        serv = in.readLine();
                         System.out.println(serv);
                     }
-                    String text = reader.readLine();
-                    out.println(text);
-                    if (text.equalsIgnoreCase("q")) {
-                        if(in.read() > -1) {
-                            String serv = in.readLine();
-                            System.out.println(serv);
-                        }
-                        break;
-                        }
+                    break;
                 }
-            } finally {
-                System.out.println("Client closed...");
-                clientSocket.close();
-                in.close();
-                out.close();
             }
-
         } catch (Exception err) {
             System.out.println("!!!!!!! = " + err);
+        } finally {
+            System.out.println("Client closed...");
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,76 +3,64 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    private static String username = "";
     private static final int PORT = 8080;
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
-    private static PrintWriter out;
-    private static BufferedReader in;
 
     public static void main(String[] args) {
-        int index = 0;
-        try {
-            try {
-                serverSocket = new ServerSocket(PORT);
-                System.out.println("Server started");
-                clientSocket = serverSocket.accept(); // ждем подключения
-                try {
-                    while (true){
-                        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
-                        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
-                        while(!clientSocket.isClosed()) {
-                            if (index == 0) {
-                                out.println("Write your name");
-                            }
-                            if (index == 1) {
-                                out.println("Are you child? (yes/no)");
-                            }
-                            if (index == 2) {
-                                out.println("Welcome to the kids area, " + username + "! Let's play! Press <q> - Exit");
-                            }
-                            if (index == 3) {
-                                out.println("Welcome to the adult zone, " + username + "! Have a good rest, or a good working day! Press <q> - Exit");
-                            }
-                            if (index > 3) {
-                                out.println(username + "! Press <q> - Exit");
-                            }
-                            String text = in.readLine();// ждём пока клиент что-нибудь нам напишет
-                            if (text.equalsIgnoreCase("q")) {
-                                out.println("Server reply - " + text + " - OK");
+        try (ServerSocket serverSocket = new ServerSocket(PORT);) {
+            System.out.println("Server started");
+            while (true) {
+                int index = 0;
+                String username = "";
+                try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                     PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+                    System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+                    while (!clientSocket.isClosed()) {
+                        if (index == 0) {
+                            out.println("Write your name");
+                        }
+                        if (index == 1) {
+                            out.println("Are you child? (yes/no)");
+                        }
+                        if (index == 2) {
+                            out.println("Welcome to the kids area, " + username + "! Let's play! Press <q> - Exit");
+                        }
+                        if (index == 3) {
+                            out.println("Welcome to the adult zone, " + username + "! Have a good rest, or a good working day! Press <q> - Exit");
+                        }
+                        if (index > 3) {
+                            out.println(username + "! Press <q> - Exit");
+                        }
+                        String text = in.readLine();// ждём пока клиент что-нибудь нам напишет
+                        if (text.equalsIgnoreCase("q")) {
+                            out.println("Server reply - " + text + " - OK");
+                            break;
+                        }
+                        System.out.println("Incoming request - " + text);
+                        switch (index) {
+                            case 0: {
+                                username = text;
+                                index = 1;
                                 break;
                             }
-                            System.out.println("Incoming request - " + text);
-                            switch (index) {
-                                case 0: {
-                                    username = text;
-                                    index = 1;
-                                    break;
+                            case 1: {
+                                if (text.equalsIgnoreCase("no")) {
+                                    index = 3;
+                                } else {
+                                    index = 2;
                                 }
-                                case 1:{
-                                    if (text.equalsIgnoreCase("no")) {
-                                        index = 3;
-                                    } else {
-                                        index = 2;
-                                    }
-                                    break;
-                                }
-                                default: index++;
+                                break;
                             }
+                            default:
+                                index++;
                         }
                     }
-                } finally {
-                    clientSocket.close();
-                    out.close();
-                    in.close();
                 }
-            } finally {
-                System.out.println("Server closed!");
-                serverSocket.close();
             }
         } catch (Exception err) {
             System.out.println("!!!!!!! = " + err);
+        } finally {
+            System.out.println("Server closed!");
         }
     }
 }
